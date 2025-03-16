91c6a99452ac3ae62287c6504b6cd4c5ddec793d
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-03-16 12:29:49 -0300

    1192 solution

--- 1192.py
+++ 1192.py
@@ -3,4 +3,35 @@ from typing import List
 
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
-        pass
+        self.time = 0
+        lowlink = [-1] * n
+        indexes = [-1] * n
+        bridges = []
+        adj = [[] for _ in range(n)]
+
+        for u, v in connections:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        def dfs(u, parent):
+            indexes[u] = lowlink[u] = self.time
+            self.time += 1
+
+            for v in adj[u]:
+                if v == parent:
+                    continue
+
+                if indexes[v] == -1:
+                    dfs(v, u)
+
+                if indexes[u] < lowlink[v]:
+                    bridges.append([u, v])
+                else:
+                    lowlink[u] = min(lowlink[u], lowlink[v])
+            return
+
+        for u, idx in enumerate(indexes):
+            if idx == -1:
+                dfs(u, None)
+
+        return bridges
